3d2101e1aa8359648df12fb1b6053d5e2d648813
frkakl <o.farukakal@gmail.com>
2023-02-21 14:57:47 +0300

    .

--- Steam_scrape_API.py
+++ Steam_scrape_API.py
@@ -12,6 +12,7 @@ df = pd.DataFrame.from_dict(json, orient='index')
 # get the rest of the pages
 for i in range(1, 999999999):
     response = requests.get("https://steamspy.com/api.php?request=all&page={}".format(i))
+    
     # check if the page is OK
     if response.status_code == 200:
         print("Page {} is OK".format(i))
@@ -19,6 +20,7 @@ for i in range(1, 999999999):
         df1 = pd.DataFrame.from_dict(json, orient='index')
         frame = [df, df1]
         df =pd.concat(frame)
+
     # if the page is not OK, stop the loop
     else:
         print("Page {} is not OK".format(i))
