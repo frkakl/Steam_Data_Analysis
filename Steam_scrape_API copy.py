8d8d777176725efb5dc0023e14a9f364ddb116d1
frkakl <o.farukakal@gmail.com>
2023-02-22 14:40:30 +0300

    .

---
+++ Steam_scrape_API copy.py
@@ -0,0 +1,35 @@
+# import libraries
+import csv
+import requests
+import pandas as pd
+
+# get the first page
+response = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json")
+print(response.status_code)
+json = response.json()
+df = pd.DataFrame.from_dict(json, orient='index')
+app_list = df[['appid', 'name']].sort_values('appid').reset_index(drop=True)
+
+
+# # get the rest of the pages
+# for i in range(1, 999999999):
+#     response = requests.get("https://steamspy.com/api.php?request=all&page={}".format(i))
+
+#     # check if the page is OK
+#     if response.status_code == 200:
+#         print("Page {} is OK".format(i))
+#         json = response.json()
+#         df1 = pd.DataFrame.from_dict(json, orient='index')
+#         frame = [df, df1]
+#         df =pd.concat(frame)
+
+#     # if the page is not OK, stop the loop
+#     else:
+#         print("Page {} is not OK".format(i))
+#         print("Scrape stopped at page {}".format(i-1))
+#         break
+
+# export as csv
+app_list.to_csv('app_list2.csv', index=False)
+
+#API_Source: https://steamspy.com/
